8538dd1d5895aa1628e5f89e24a83c81a9be7b2e
Mike Jones <mike@westerncapelabs.com>
2013-08-30 17:05:27 +0200

    Updated to first unreg states from skeleton

--- test/test-go-grs-mobilegateway.js
+++ test/test-go-grs-mobilegateway.js
@@ -18,58 +18,99 @@ describe("test_api", function() {
     });
 });
 
-// YOUR TESTS START HERE
-// CHANGE THIS to test_your_app_name
-describe("When using the skeleton", function() {
+// These are used to mock API reponses
+// EXAMPLE: Response from google maps API
+var test_fixtures_full = [
+];
 
-    // These are used to mock API reponses
-    // EXAMPLE: Response from google maps API
-    var fixtures = [
-       'test/fixtures/example-geolocation.json'
-    ];
+var tester;
 
-    var tester = new vumigo.test_utils.ImTester(app.api, {
-        custom_setup: function (api) {
-            api.config_store.config = JSON.stringify({
-                //user_store: "go_skeleton"
-            });
-            fixtures.forEach(function (f) {
-                api.load_http_fixture(f);
-            });
-        },
-        async: true
+describe("When using the USSD line as an un registered user", function() {
+
+    var fixtures = test_fixtures_full;
+    beforeEach(function() {
+        tester = new vumigo.test_utils.ImTester(app.api, {
+            custom_setup: function (api) {
+                api.config_store.config = JSON.stringify({
+                    cms_api_root: 'http://qa/api/v1/'
+                });
+
+                var dummy_contact = {
+                    key: "f953710a2472447591bd59e906dc2c26",
+                    surname: "Trotter",
+                    user_account: "test-0-user",
+                    bbm_pin: null,
+                    msisdn: "+1234567",
+                    created_at: "2013-04-24 14:01:41.803693",
+                    gtalk_id: null,
+                    dob: null,
+                    groups: null,
+                    facebook_id: null,
+                    twitter_handle: null,
+                    email_address: null,
+                    name: "Rodney"
+                };
+
+                api.add_contact(dummy_contact);
+
+                fixtures.forEach(function (f) {
+                    api.load_http_fixture(f);
+                });
+            },
+            async: true
+        });
     });
 
     // first test should always start 'null, null' because we haven't
     // started interacting yet
-    it("first screen should ask us to say something ", function (done) {
+    it("should be asked to answer registration questions", function (done) {
         var p = tester.check_state({
             user: null,
             content: null,
             next_state: "first_state",
-            response: "^Say something please"
+            response: "^Welcome Sisi! Is it OK if Coach Tumi from SKILLZ Street " +
+                "asks you a few questions\\?[^]" +
+                "1. Yes[^]" +
+                "2. No$"
         });
         p.then(done, done);
     });
 
-    it("second screen should ask if we want to know what we said", function (done) {
+    it("answering no to registration should ask exit", function (done) {
         var user = {
             current_state: 'first_state'
         };
         var p = tester.check_state({
             user: user,
-            content: "Hello world!",
-            next_state: "second_state",
+            content: "2",
+            next_state: "reg_end_error",
             response: (
-                "^Thank you! Do you what to know what you said\\?[^]" +
-                "1. Yes[^]"+
-                "2. No$"
+                "^Thank you for stopping by! Feel free to call Coach Tumi any " +
+                    "time. Kilo for strong and beautiful girls! Love, Coach Tumi$"
+            ),
+            continue_session: false
+        });
+        p.then(done, done);
+    });
+
+    it("answering yes to registration should ask sex", function (done) {
+        var user = {
+            current_state: 'first_state'
+        };
+        var p = tester.check_state({
+            user: user,
+            content: "1",
+            next_state: "reg_sex",
+            response: (
+                "^What is your sex\\?[^]" +
+                "1. Female[^]" +
+                "2. Male$"
             )
         });
         p.then(done, done);
     });
 
-    it("declining to know what we said, should say goodbye", function (done) {
+    it.skip("declining to know what we said, should say goodbye", function (done) {
         var user = {
             current_state: 'second_state',
             answers: {
@@ -84,70 +125,6 @@ describe("When using the skeleton", function() {
             continue_session: false
         });
         p.then(done, done);
-    });
-
-    it("agreeing to know what we said should show us", function (done) {
-        var user = {
-            current_state: 'second_state',
-            answers: {
-                first_state: 'Hello world!'
-            }
-        };
-        var p = tester.check_state({
-            user: user,
-            content: "1",
-            next_state: "third_state",
-            response: (
-                "^We think you said 'Hello world!'. Correct\\?[^]" +
-                "1. Yes[^]"+
-                "2. No$"
-            )
-        });
-        p.then(done, done);
-    });
-
-    it("told we got it right so say goodbye", function (done) {
-        var user = {
-            current_state: 'third_state',
-            answers: {
-                first_state: 'Hello world!',
-                second_state: 'third_state'
-            }
-        };
-        var p = tester.check_state({
-            user: user,
-            content: "1",
-            next_state: "end_state_correct",
-            response: "^Aren't we clever\\? Thank you and bye bye!$",
-            continue_session: false
-        });
-        p.then(done, done);
-    });
-
-    it("told we got it wrong so say goodbye", function (done) {
-        var user = {
-            current_state: 'third_state',
-            answers: {
-                first_state: 'Hello world!',
-                second_state: 'third_state'
-            }
-        };
-        var p = tester.check_state({
-            user: user,
-            content: "2",
-            next_state: "end_state_wrong",
-            response: "^Silly us! Thank you and bye bye!$",
-            continue_session: false
-        });
-        p.then(done, done);
-    });
-
-
-    // This is an example of a test that we don't want to run at the moment
-    // so we add .skip
-    it.skip('should go to end when asked for them to say someting', function() {
-        var p = tester.check_state({current_state: 'state_we_have_not_made'}, 'Hello world!',
-            'end_state', '^Thank you and bye bye!');
     });
 
 });
